ad2e1917cf4e0be5c994efdf8b85da6687d820d1
Nejc Selan <nejc.selan19@gmail.com>
2021-09-07 21:59:27 +0200

    adding multer for uploading files

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ mongoose.connect(db, { useNewUrlParser: true })
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// Public folder
+app.use(express.static('./public'));
+
 // Bodyparser
 app.use(express.urlencoded({extended: false}));
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const { ensureAuthenticated } = require('../config/auth');
+
 
 router.get('/', (req,res) => res.render('home'));
 router.get('/dashboard', ensureAuthenticated, (req,res) =>
@@ -8,5 +11,60 @@ router.get('/dashboard', ensureAuthenticated, (req,res) =>
         name: req.user.name
     })
 );
+
+// upload stuff
+// Set storage engine - Multer
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function (req, file, cb) {
+        cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+})
+// Init upload
+const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single('profilePicture');
+// Check file type
+function checkFileType(file, cb) {
+    // Allowed extensions
+    const fileTypes = /jpeg|jpg|png|gif/;
+    // Check ext
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+    const mimetype = fileTypes.test(file.mimetype);
+
+    if(mimetype && extname){
+        return cb(null, true);
+    } else {
+        cb('Error: Only images can be uploaded');
+    }
+}
+
+
+// Upload handler
+router.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if(err){
+            res.render('dashboard', {
+                msg: err
+            })
+        } else {
+            if(!req.file){
+                res.render('dashboard', {
+                    msg: 'Error: No file selected'
+                });
+            } else {
+                res.render('dashboard', {
+                    msg: 'File uploaded!',
+                    image: `uploads/${req.file.filename}`
+                })
+            }
+        }
+    });
+})
 
 module.exports = router;
